143749ce60b7440d5304aedf6e7b49a7de863b61
cjoraeki <cjoraeki3@gmail.com>
2023-02-16 14:00:12 +0100

    cicd configuration and dockerisation

--- src/main/java/com/example/ewalletapi/EWalletApiApplication.java
+++ src/main/java/com/example/ewalletapi/EWalletApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EWalletApiApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(EWalletApiApplication.class, args);
     }
